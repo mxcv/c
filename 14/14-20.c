329d503ff520ff0dc7b57e4bfff5a61d46103ff6
Max <unfusions@gmail.com>
2020-05-25 22:34:07 +0300

    bool

--- 14/14-20.c
+++ 14/14-20.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <conio.h>
 #include <Windows.h>
 #include "menu.h"
--- 14/14-9.c
+++ 14/14-9.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <conio.h>
 #include <time.h>
 #include <Windows.h>
